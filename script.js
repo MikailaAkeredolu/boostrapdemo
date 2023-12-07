81fc6218212338bff82435f73bf6dc7e9b1b1062
mikailaAkeredolu <mikaila@tech552.com>
2023-12-07 12:30:57 -0500

    bootsrap lecture for cohort14

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const button = document.getElementById('butt');
+button.addEventListener('click',  ()=>{
+    // alert(' Welcome to my onlyfans ');
+    document.body.style.backgroundColor = 'pink';
+} );
